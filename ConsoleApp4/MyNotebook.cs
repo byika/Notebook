f5bb64deb2d7f85b75631a5b264347998089e431
Andrei Kostin <andrejkostin2005@gmail.com>
2023-06-20 19:08:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/MyNotebook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotebook
+{
+    public class Notebook
+    {
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void on()
+        {
+            Console.WriteLine("ВЖВВЖВЖЖЖЖЖЖЖЖЖВЖВЖЖЖЖЖЖЖЖ (куллер работает на максимум)");
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyNotebook;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook();
+            notebook.Model = "Модель ноутбука";
+            notebook.Year = 2023;
+            notebook.on();
+
+            Console.ReadLine();
+
+        }
+    }
+}
